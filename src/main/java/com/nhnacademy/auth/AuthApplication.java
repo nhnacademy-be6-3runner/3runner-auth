5859110133eed7e0107acafbf9bb9c72cad37e8a
administrator <administrator@administrators-Laptop.local>
2024-07-04 15:32:04 +0900

    fix:Main name fix

--- src/main/java/com/nhnacademy/auth/AuthApplication.java
+++ src/main/java/com/nhnacademy/auth/AuthApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
-public class Application {
+public class AuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 
 }
